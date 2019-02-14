adb1b82118773bc168b9e59108e1104966a3c2b8
Eason <eason.sy.liu@foxconn.com>
2019-02-14 16:52:11 +0800

    89

--- 89. Gray Code/grayCode.c
+++ 89. Gray Code/grayCode.c
@@ -7,46 +7,18 @@
  */
 int* grayCode(int n, int* returnSize) {
     int *output;
-	int idx = 0;
-	int *used;
-	int currsize = 0;
-	int curridx = 0;
-	int value = 1;
-    int tmp_value = 0;
+    int value = 0x1;
 
     *returnSize = 1 << n;
     output = malloc((*returnSize) * sizeof(int));
-	used = malloc((*returnSize) * sizeof(int));
+	
+    output[0] = 0;
 
-	memset(used, 0x0, (*returnSize) * sizeof(int));
-
-	do {
-		int one_count = 0;
-
-		while (used[curridx]) {
-			curridx++;
-		}
-
-		tmp_value = value ^ curridx;
-
-		while ((tmp_value != 0) || (one_count <= 1)) {
-			one_count += tmp_value & 0x1;
-			tmp_value = tmp_value >> 1;
-		}
-
-		if (one_count == 1) {
-			output[idx] = curridx;
-            value = curridx;
-			idx++;
-			used[curridx] = 1;
-            curridx = 0;
-			currsize++;
-		} else {
-            curridx++;
+    for (int i = 0; i < n; i++) {
+        for (int j = (1 << i), m = 1; j < (1 << (i + 1)); j++, m++) {
+            output[j] = (1 << i) | output[(1 << i) - m];
         }
-	} while (currsize != *returnSize);
-
-	free(used);
+    }
 
     return output;
 }

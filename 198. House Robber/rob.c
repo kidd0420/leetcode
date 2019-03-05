ddb3cd8d077a41da6868cf1181b7d61472edf3ae
Eason <eason.sy.liu@foxconn.com>
2019-03-05 17:30:57 +0800

    198

--- 198. House Robber/rob.c
+++ 198. House Robber/rob.c
@@ -1,7 +1,11 @@
 /*
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+You are a professional robber planning to rob houses along a street. 
+Each house has a certain amount of money stashed, 
+the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and 
+it will automatically contact the police if two adjacent houses were broken into on the same night.
 
-Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+Given a list of non-negative integers representing the amount of money of each house, 
+determine the maximum amount of money you can rob tonight without alerting the police.
 
 Example 1:
 
@@ -16,7 +20,61 @@ Output: 12
 Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
              Total amount you can rob = 2 + 9 + 1 = 12.
 */
+#include "stdio.h"
+#include <stdlib.h>
 
 int rob(int* nums, int numsSize) {
-    
+    int *add_nums;
+	int i,j;
+	int tmp_num;
+
+	if (numsSize == 1) {
+		return nums[0];
+	} else if (numsSize == 2) {
+		if (nums[0] > nums[1])
+			return nums[0];
+		else
+			return nums[1];
+	}
+
+	add_nums = malloc(sizeof(int) * numsSize);
+	memset(add_nums, 0, sizeof(int) * numsSize);
+
+	for (i = 0; i < numsSize - 2; i++) {
+		for (j = 2; j < 4; j++) {
+			if (i + j >= numsSize)
+				break;
+
+			if (nums[i] > add_nums[i])
+				tmp_num = nums[i + j] + nums[i];
+			else
+				tmp_num = nums[i + j] + add_nums[i];
+
+			if (tmp_num > add_nums[i + j])
+				add_nums[i + j] = tmp_num;
+		}
+	}
+
+	if (add_nums[numsSize - 2] > add_nums[numsSize - 1])
+		tmp_num = add_nums[numsSize - 2];
+	else
+		tmp_num = add_nums[numsSize - 1];
+	
+	free(add_nums);
+
+	return tmp_num;
+}
+
+
+int main ()
+{
+    int input[] = {8,3,1,4,5,9};
+    int count = sizeof(input) / sizeof(int);
+    int sum;
+
+    sum = rob(input, count);
+
+    printf("sum: %d\n", sum);
+
+    return 0;
 }

c79ecfdf51d9ac43a6d46d7b5c4b0bab364bdaba
Eason <eason.sy.liu@foxconn.com>
2019-03-21 09:47:17 +0800

    931

--- 931. Minimum Falling Path Sum/minFallingPathSum.c
+++ 931. Minimum Falling Path Sum/minFallingPathSum.c
@@ -18,7 +18,66 @@
  * 1 <= A.length == A[0].length <= 100
  * -100 <= A[i][j] <= 100
  *********************************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define min(x,y)         (x < y?x:y)
+
 
 int minFallingPathSum(int** A, int ARowSize, int *AColSizes) {
-    
+	int count = ARowSize - 1;
+	int i;
+	int min_sum;
+
+	if (ARowSize == 1)
+		return A[0][0];
+
+    while (count > 0) {
+    	for (i = 0; i < *AColSizes; i++) {
+    		if (i == 0) {
+    			A[count - 1][i] += min(A[count][i], A[count][i + 1]);
+    		} else if (i == (*AColSizes) - 1) {
+    			A[count - 1][i] += min(A[count][i - 1], A[count][i]);
+    		} else {
+    			int tmp;
+
+    			tmp = min(A[count][i - 1], A[count][i]);
+    			tmp = min(tmp, A[count][i + 1]);
+    			A[count - 1][i] += tmp;
+    		}
+    	}
+
+        count--;
+    }
+
+    min_sum = A[0][0];
+
+    for (i = 1; i < *AColSizes; i++) {
+    	min_sum = min(min_sum, A[0][i]);
+    }
+
+    return min_sum;
+}
+
+
+int main ()
+{
+    int **input;
+    int input_0[3] = {1,2,3};
+    int input_1[3] = {4,5,6};
+    int input_2[3] = {7,8,9};
+    int col = 3;
+    int min;
+
+    input = malloc(3 * sizeof(int*));
+
+    input[0] = input_0;
+    input[1] = input_1;
+    input[2] = input_2;
+
+    min = minFallingPathSum(input, 3, &col);
+
+    printf("min: %d\n", min);
+
+    return 0;
 }

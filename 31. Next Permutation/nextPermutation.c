8a1f322cdc2ca2e23bc01266905cf9674dae5bbb
Eason <eason.sy.liu@foxconn.com>
2019-03-21 15:36:11 +0800

    31

--- 31. Next Permutation/nextPermutation.c
+++ 31. Next Permutation/nextPermutation.c
@@ -14,7 +14,86 @@
  * 3,2,1 → 1,2,3
  * 1,1,5 → 1,5,1
  ***********************************************************************************/
+#include <stdio.h>
+
+
+void Revert(int* nums, int numsSize) {
+	int tmp;
+	int i;
+
+	for (i = 0; i < numsSize / 2; i++) {
+		tmp = nums[(numsSize - 1) - i];
+		nums[(numsSize - 1) - i] = nums[i];
+		nums[i] = tmp;
+	}
+}
+
+
+void Reorder(int* nums, int numsSize, int idx) {
+	int tmp;
+
+	if (idx == numsSize - 1) {
+		tmp = nums[idx - 1];
+		nums[idx - 1] = nums[idx];
+		nums[idx] = tmp;
+	} else {
+		int i;
+
+		for (i = numsSize - 1; i > idx; i--) {
+			if (nums[idx - 1] < nums[i]) {
+				tmp = nums[i];
+				nums[i] = nums[idx - 1];
+				nums[idx - 1] = tmp;
+				Revert(&nums[idx], numsSize - idx);
+				return;
+			}
+		}
+	}
+}
+
 
 void nextPermutation(int* nums, int numsSize) {
-    
+	int i;
+
+	if (numsSize <= 1)
+		return;
+
+	for (i = numsSize - 1; i > 0; i--) {
+		if (nums[i] > nums[i - 1]) {
+			Reorder(nums, numsSize, i);
+			return;
+		}
+	}
+
+	Revert(nums, numsSize);
+}
+
+
+void ShowArray(int* nums, int numsSize)
+{
+    int i;
+
+    printf("[");
+
+    for (i = 0; i < numsSize; i++) {
+        printf("%d", nums[i]);
+
+        if (i != numsSize -1)
+            printf(",");
+    }
+
+    printf("]\n");
+}
+
+
+int main ()
+{
+    int input[6] = {5,4,4,4,2,1};
+    int size = sizeof(input) / sizeof(int);
+
+    ShowArray(input, size);
+    nextPermutation(input, size);
+    ShowArray(input, size);
+
+    return 0;
 }
